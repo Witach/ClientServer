cb93f2e38f2d2c3a7ee7a19357b65e78738a6fa5
215920@edu.p.lodz.pl <witaszek98@wp.pl>
2019-05-15 19:28:09 +0200

    project started

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+}
